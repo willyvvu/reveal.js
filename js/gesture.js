1e596e94491d6b3300fbe8bc1aee3e4510279155
Trey Hunner <trey@treyhunner.com>
2015-05-04 21:26:06 -0700

    Replace stray mixed tabs/spaces with tabs

--- js/gesture.js
+++ js/gesture.js
@@ -46,28 +46,28 @@ function skinfilter(){
 		{
 			index_value = x+y*width
 			r = draw.data[count_data_big_array]
-            		g = draw.data[count_data_big_array+1]
-            		b = draw.data[count_data_big_array+2]
-            		a = draw.data[count_data_big_array+3]
+			g = draw.data[count_data_big_array+1]
+			b = draw.data[count_data_big_array+2]
+			a = draw.data[count_data_big_array+3]
 
-            		hsv = rgb2Hsv(r,g,b);
-            		//When the hand is too lose (hsv[0] > 0.59 && hsv[0] < 1.0)
+			hsv = rgb2Hsv(r,g,b);
+			//When the hand is too lose (hsv[0] > 0.59 && hsv[0] < 1.0)
 			//Skin Range on HSV values
 			if(((hsv[0] > huemin && hsv[0] < huemax)||(hsv[0] > 0.59 && hsv[0] < 1.0))&&(hsv[1] > satmin && hsv[1] < satmax)&&(hsv[2] > valmin && hsv[2] < valmax)){
 
-	       			skin_filter[count_data_big_array]=r
+				skin_filter[count_data_big_array]=r
 				skin_filter[count_data_big_array+1]=g
 				skin_filter[count_data_big_array+2]=b
 				skin_filter[count_data_big_array+3]=a
-	        	}else{
+			}else{
 
-	        		skin_filter.data[count_data_big_array]=
+				skin_filter.data[count_data_big_array]=
 				skin_filter.data[count_data_big_array+1]=
 				skin_filter.data[count_data_big_array+2]=0
 				skin_filter.data[count_data_big_array+3]=0
-	        	}
+			}
 
-            		count_data_big_array=index_value*4;
+			count_data_big_array=index_value*4;
 		}
 	}
 	draw=skin_filter
@@ -75,32 +75,32 @@ function skinfilter(){
 
 function rgb2Hsv(r, g, b){
 	
-    r = r/255
-    g = g/255
-    b = b/255;
+	r = r/255
+	g = g/255
+	b = b/255;
 
-    var max = Math.max(r, g, b)
-    var min = Math.min(r, g, b);
+	var max = Math.max(r, g, b)
+	var min = Math.min(r, g, b);
 
-    var h, s, v = max;
+	var h, s, v = max;
 
-    var d = max - min;
+	var d = max - min;
 
-    s = max == 0 ? 0 : d / max;
+	s = max == 0 ? 0 : d / max;
 
-    if(max == min){
-        h = 0; // achromatic
-    }else{
+	if(max == min){
+		h = 0; // achromatic
+	}else{
 
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-    	}
-   		h /= 6;
-   	}
+		switch(max){
+			case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+			case g: h = (b - r) / d + 2; break;
+			case b: h = (r - g) / d + 4; break;
+		}
+		h /= 6;
+	}
 
-    return [h, s, v];
+	return [h, s, v];
 }
 
 last=false
